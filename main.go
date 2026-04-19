dc7403e7d9da29f3c0645d4166dd9b4ee81408f1
lotacodic <ngenejoshua05@gmail.com>
2026-04-20 00:31:14 +0100

    initial commit: project setup

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func isValid(grid [][]int, row, col, num int) bool {
+
+}
+
+func isGridValid(grid [][]int) bool {
+
+}
+
+
+func solve(grid [][]int) bool {
+
+}
+
+func main() {
+
+}
